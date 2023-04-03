c6cac2eae5656a55496de03c9ec1d0c17f8db250
DevopsEasy <48271676+DevopsEasy@users.noreply.github.com>
2023-04-03 12:59:49 +0530

    added day 7 files

---
+++ terraformfiles/day7/network.tf
@@ -0,0 +1,19 @@
+
+resource "aws_vpc" "primary_vpc" {
+  cidr_block = var.vpc_cidr
+  tags = {
+      Name = "primary"
+  }
+}
+
+resource "aws_subnet" "subnets" {
+  count = length(var.subnet_cidrs)
+  vpc_id = aws_vpc.primary_vpc.id
+  availability_zone = var.subnet_azs[count.index]
+  cidr_block = var.subnet_cidrs[count.index]
+
+  tags = {
+      Name = var.subnet_names[count.index]
+  }
+  
+}
